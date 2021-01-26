f0014922d5c54092e5b0421dc7c6def34f1b59e9
Caroline Caprini <caroline.caprini@hotmail.com>
2021-01-25 21:59:11 -0300

    UserController Updated

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -28,12 +28,8 @@ class UserController extends AdminController
 
         $grid->column('id', __('Id'));
         $grid->column('name', __('Name'));
+        $grid->column('user', __('User'));
         $grid->column('email', __('Email'));
-        $grid->column('email_verified_at', __('Email verified at'));
-        $grid->column('password', __('Password'));
-        $grid->column('remember_token', __('Remember token'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
 
         return $grid;
     }
@@ -50,10 +46,9 @@ class UserController extends AdminController
 
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
+        $show->field('user', __('User'));
         $show->field('email', __('Email'));
         $show->field('email_verified_at', __('Email verified at'));
-        $show->field('password', __('Password'));
-        $show->field('remember_token', __('Remember token'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -70,10 +65,9 @@ class UserController extends AdminController
         $form = new Form(new User());
 
         $form->text('name', __('Name'));
+        $form->text('user', __('User'));
         $form->email('email', __('Email'));
-        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
         $form->password('password', __('Password'));
-        $form->text('remember_token', __('Remember token'));
 
         return $form;
     }
